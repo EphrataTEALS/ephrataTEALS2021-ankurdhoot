16c7f6f211150d82ad664464c2db95c3c7138788
ankurdhoot <ankurdhoot97@gmail.com>
2021-09-23 15:54:43 +0000

    Modifying StarFigures.java

---
+++ sampleCode/Chapter1/StarFigures.java
@@ -0,0 +1,53 @@
+public class StarFigures {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println();
+
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println();
+
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println();
+
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+    }
+
+    public static void drawOneStar() {
+        System.out.println("  *  ");
+    }
+
+    public static void drawTwoStars() {
+        System.out.println(" * * ");
+    }
+
+    public static void drawFiveStars() {
+        System.out.println("*****");
+    }
+
+    public static void drawShape() {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println();
+    }
+}
